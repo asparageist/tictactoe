100e01764265d166bd74ba8cd45614758e2cc440
Joseph Murray <josephwmurray@gmail.com>
2023-07-10 19:40:27 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let cells = document.querySelectorAll('.cell');
+
+cells.forEach(function(cell) {
+  cell.addEventListener('click', function() {
+    const makeX = document.createElement("p");
+    makeX.append("X");
+    cell.append(makeX);
+  })
+})
