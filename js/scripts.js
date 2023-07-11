19d304eb4f6058646f8f8a8566d776e72315da72
Joseph Murray <josephwmurray@gmail.com>
2023-07-10 20:49:36 -0700

    add functionality to switch players and establish arrays of clicked boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,46 @@
 let cells = document.querySelectorAll('.cell');
+let player = "X";
+let xWin = [];
+let oWin = [];
+
 
 cells.forEach(function(cell) {
-  cell.addEventListener('click', function() {
-    const makeX = document.createElement("p");
-    makeX.append("X");
-    cell.append(makeX);
-  })
-})
+  let xCell = false;
+  let oCell = false;
 
+  cell.addEventListener('click', function() {
+    if (player === "X") {
+      if (xCell === false && oCell === false) {
+        let cellNumber = cell.textContent;
+        xCell = true;
+        clickCells(cell, player);
+        player = "O";
+        xWin.push(cellNumber);
+        console.log(xWin);
+      } 
+    } else if (player === "O") {
+      if (xCell === false && oCell === false) {
+        let cellNumber = cell.textContent;
+        oCell = true;
+        clickCells(cell, player);
+        player = "X";
+        oWin.push(cellNumber);
+        console.log(oWin);
+      }
+  }
+  checkWin();
+  });
+});
+
+
+function clickCells(cell, player) {
+  const takeBox = document.createElement("p");
+  takeBox.append(player);
+  cell.append(takeBox);
+}
+
+function checkWin () {
+  let win1 = ['1', '2', '3'];
+  let checkX = xWin.includes(win1);
+  console.log(xWin.includes(win1));
+  }
